dffc3095aac6ad791a6c4e73164ac0e93ceff3df
Cox <jcox@NATL2236LJCOX.local>
2016-09-29 21:47:49 -0400

    send message and redirect only if file already exists

--- upload/views.py
+++ upload/views.py
@@ -11,21 +11,24 @@ from django.template import RequestContext
 import boto3, botocore
 import csvkit
 
+BUCKET_NAME = 'ajc-data-warehouse'
+
 def upload_file(request):
     if request.method == 'POST':
-        messages.add_message(request, messages.ERROR, 'A file with that name already exists')
-        return HttpResponseRedirect('/')
         fcontent = request.FILES['file-input'].read()
         fkey = request.FILES['file-input'].name
 
         # Access bucket using credentials in ~/.aws/credentials
         s3 = boto3.resource('s3')
-        bucket = s3.Bucket('ajc-data-warehouse')
+        bucket = s3.Bucket(BUCKET_NAME)
 
         # Check if a file with the same name already exists in the
-        # S3 bucket, and if so throw an error
+        # S3 bucket, and if so throw an error and return to the index
+        # page
         try:
             bucket.download_file(fkey, '/tmp/boto_test')
+            messages.add_message(request, messages.ERROR, 'A file with that name already exists')
+            return HttpResponseRedirect('/')
         except botocore.exceptions.ClientError:
             pass
 
@@ -39,7 +42,8 @@ def upload_file(request):
 
         # Return a preview of the top few rows in the table
         # and check if the casting is correct
-        # Return a log of the issues that need to be fixed
+        # After running the create table query, return a
+        # log of the issues that need to be fixed if any
 
         # Write the file to Amazon S3
         bucket.put_object(Key=fkey, Body=fcontent)
