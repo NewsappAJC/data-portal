7e31c68692618d3f62d7d20f202fe1bfa722d048
Cox <jcox@NATL2236LJCOX.local>
2016-09-30 12:47:38 -0400

    added example my.cnf file

--- data_import_tool/settings.py
+++ data_import_tool/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'data_import_tool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf')
+            }
     }
 }
 
---
+++ my.cnf.ex
@@ -0,0 +1,7 @@
+;Rename this file to my.cnf and add your data
+[client]
+database=
+user=
+password=
+port=3306
+default-character-set=utf8
