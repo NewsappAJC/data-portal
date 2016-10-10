cd2fe821065994e3d2b67b320cedb5a570688cbe
jernsthausen <jernsthausen@ajc.com>
2016-10-10 16:24:49 -0400

    create function to polish create table statemnts

--- upload/utils.py
+++ upload/utils.py
@@ -1,6 +1,9 @@
 # Jeff just put the functions here and we can import them in
 # tasks.py 
 
+#remove quote chars csvkit puts around capitalized column names, change engine to MYISAM
+#if routine errors occur, use this to modify the create table statement
+#deprecate when we start interactively handling field types
 def polish_create_table(create_table):
     create_table=create_table.replace('"','')
     
