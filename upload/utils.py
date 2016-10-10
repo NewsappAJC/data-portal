3b9fc691896fab80ebefdc064bc7af02c507e51b
jernsthausen <jernsthausen@ajc.com>
2016-10-10 16:22:49 -0400

    create function to polish create table

--- upload/utils.py
+++ upload/utils.py
@@ -1,2 +1,9 @@
 # Jeff just put the functions here and we can import them in
 # tasks.py 
+
+def polish_create_table(create_table):
+    create_table=create_table.replace('"','')
+    
+    create_table=create_table.replace(");",")ENGINE=MYISAM;")
+    
+    return create_table
