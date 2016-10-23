1ab44b1faf0b72e4925be135d9798ef8f7938ec2
Cox <jcox@NATL2236LJCOX.local>
2016-10-23 16:55:29 -0400

    form layout better but upload broken

--- upload/forms.py
+++ upload/forms.py
@@ -1,7 +1,17 @@
+# Standard library imports
 import re
+import os
 
+# Third party imports
 from django import forms
 from django.forms import widgets
+
+# Local module imports
+import sqlalchemy
+from sqlalchemy.engine.url import make_url
+
+# Constants
+URL = os.environ['DATA_WAREHOUSE_URL']
 
 REPORTERS = (
     ('Jonathan Cox', 'Jonathan Cox'),
@@ -16,21 +26,35 @@ DELIMITERS = (
 )
 
 class DataForm(forms.Form):
-    # Set classes on form inputs
     def __init__(self, *args, **kwargs):
         super(DataForm, self).__init__(*args, **kwargs)
+
+        # Get database options TODO maybe there's a better way to do this
+        # than running an expensive operation like connecting to the server?
+        engine = sqlalchemy.create_engine(URL)
+        connection = engine.connect()
+        data = connection.execute('SHOW DATABASES;')
+
+        databases = []
+        for db in data:
+            pair = list(db)
+            pair.extend(list(db))
+            databases.append(pair)
+
+        self.fields['db_name'] = forms.ChoiceField(label='Database', choices=databases, initial='user_cox')
+
+        # Set classes on form inputs
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
 
     data_file = forms.FileField(label='File')
+    table_name = forms.SlugField(label='Table name', max_length=100)
     delimiter = forms.ChoiceField(label='Delimiter', choices=DELIMITERS, initial=',')
-    db_name = forms.CharField(label='Database name', max_length=100, initial='user_cox')
-    table_name = forms.SlugField(label='Table name', max_length=100, initial='test')
-    source = forms.CharField(label='Source', max_length=100, initial='God')
-    #topic = forms.CharField(label='Topic', max_length=100, initial='test')
-    #reporter_name = forms.ChoiceField(label='Reporter who aquired data', choices=REPORTERS, initial='Jonathan Cox')
-    #next_aquisition = forms.DateField(label='When to update data', widget=forms.SelectDateWidget, required=False)
-    #press_contact = forms.CharField(label='Press contact name', max_length=100, required=False)
-    #press_contact_number = forms.CharField(label='Press contact number', max_length=100, required=False)
-    #press_contact_email = forms.EmailField(label='Press contact email', required=False)
+    source = forms.CharField(label='Source', max_length=100, required=False)
+    topic = forms.CharField(label='Topic', max_length=100, required=False)
+    reporter_name = forms.ChoiceField(label='Reporter who aquired data', choices=REPORTERS)
+    next_aquisition = forms.DateField(label='When to update data', widget=forms.SelectDateWidget, required=False)
+    press_contact = forms.CharField(label='Press contact name', max_length=100, required=False)
+    press_contact_number = forms.CharField(label='Press contact number', max_length=100, required=False)
+    press_contact_email = forms.EmailField(label='Press contact email', required=False)
 
