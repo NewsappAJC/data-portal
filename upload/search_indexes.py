d162b729f0a66491881d5634f7e53237f434561f
Cox <jonathan.cox.c@gmail.com>
2016-12-06 11:43:31 -0500

    querystring and comments

--- upload/search_indexes.py
+++ upload/search_indexes.py
@@ -2,10 +2,14 @@ import datetime
 from haystack import indexes
 from .models import Table
 
-# Create a search index that Haystack uses to find matching results
+
 class TableIndex(indexes.SearchIndex, indexes.Indexable):
+    """Object that handles the flow of data into the search index"""
+
+    # Define the fields we want to store data with
     text = indexes.CharField(document=True, use_template=True)
 
+    # Specify what type of object the search results should return
     def get_model(self):
         return Table
 
