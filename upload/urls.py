c98b81940a995d96ed1547ba2cf86b2d7dc5b942
Cox <jonathan.cox.c@gmail.com>
2016-12-06 17:59:48 -0500

    some long-overdue namespacing and names for URLs

--- upload/urls.py
+++ upload/urls.py
@@ -4,11 +4,12 @@ from django.contrib.auth import views as auth_views
 
 from . import views
 
+app_name='upload'
 urlpatterns = [
-    url(r'^$', views.upload_file),
-    url(r'^categorize/$', views.categorize),
-    url(r'^upload/$', views.upload),
-    url(r'^check-task-status/$', views.check_task_status),
+    url(r'^$', views.upload_file, name='index'),
+    url(r'^categorize/$', views.categorize, name='categorize'),
+    url(r'^upload/$', views.write_to_db, name='upload'),
+    url(r'^check-task-status/$', views.check_task_status, name='check_status'),
     url(r'^login/$', auth_views.login),
     url(r'^logout/$', views.logout_user)
 ]
