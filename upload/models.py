e939185969a141e7dc47f285421bc78d562c1b87
Cox <jonathan.cox.c@gmail.com>
2016-12-07 17:55:05 -0500

    increased max length of url field in Table model

--- upload/models.py
+++ upload/models.py
@@ -12,7 +12,7 @@ class Table(models.Model):
     upload_log = models.TextField(blank=True)
     source = models.CharField(max_length=300, blank=False, null=False)
     next_update = models.DateField(blank=True, null=True)
-    url = models.URLField(max_length=400)
+    url = models.URLField(max_length=500)
 
 
 class Column(models.Model):
