aaf04aa399fb34866e678153db6c9d0733371744
Cox <jonathan.cox.c@gmail.com>
2016-12-07 17:56:42 -0500

    changed and added some URL names, fixed spacing

--- upload/urls.py
+++ upload/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import url
-from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 
 from . import views
 
-app_name='upload'
+app_name = 'upload'
 urlpatterns = [
     url(r'^$', views.upload_file, name='index'),
     url(r'^categorize/$', views.categorize, name='categorize'),
-    url(r'^upload/$', views.write_to_db, name='upload'),
+    url(r'^upload/$', views.write_to_db, name='write_to_db'),
     url(r'^check-task-status/$', views.check_task_status, name='check_status'),
-    url(r'^login/$', auth_views.login),
-    url(r'^logout/$', views.logout_user)
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', views.logout_user, name='logout')
 ]
