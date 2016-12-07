13690c37404ac6428b89fb4c0459a98362ee2783
Cox <jonathan.cox.c@gmail.com>
2016-12-07 12:14:50 -0500

    PEP8 syntax

--- upload/forms.py
+++ upload/forms.py
@@ -4,11 +4,9 @@ import os
 
 # Third party imports
 from django import forms
-from django.forms import widgets
 
 # Local module imports
 import sqlalchemy
-from sqlalchemy.engine.url import make_url
 
 # Constants
 URL = os.environ['DATA_WAREHOUSE_URL']
@@ -28,8 +26,10 @@ DELIMITERS = (
 # 10MB = 10485760
 MAX_UPLOAD_SIZE = 10485760
 
+
 class DataForm(forms.Form):
-    # Initialize the data upload form with a list of databases on the MySQL server
+    # Initialize the data upload form with a list of databases on the MySQL
+    # server
     def __init__(self, *args, **kwargs):
         super(DataForm, self).__init__(*args, **kwargs)
 
@@ -45,36 +45,36 @@ class DataForm(forms.Form):
             databases.append(pair)
 
         self.fields['db_select'] = forms.ChoiceField(label='Select a database',
-            choices=databases,
-            initial=None,
-            required=False)
+                                                     choices=databases,
+                                                     initial=None,
+                                                     required=False)
 
         # Set Bootstrap classes on form inputs
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-control'})
 
-    # Internal method that checks for non-alphanumeric characters and raises an error
-    # if it encounters them
+    # Internal method that checks for non-alphanumeric characters and raises an
+    # error if it encounters them
     def _sanitize(self, data):
         r = re.compile(r'\W')
-        if r.search(data) != None:
+        if r.search(data) is not None:
             raise forms.ValidationError(
                 'Only alphanumeric characters and underscores (_) are allowed.'
             )
         return data
 
-    # Customize the clean method to check that one and only one of the two DB 
+    # Customize the clean method to check that one and only one of the two DB
     # inputs contains valid data
     def clean(self):
         cleaned_data = super(DataForm, self).clean()
-        select = cleaned_data.get('db_select')
-        if not cleaned_data.get('db_input') and not cleaned_data.get('db_select'):
-            raise forms.ValidationError(
-                'Please select a database or input a new database name.'
-            )
+        db_input = cleaned_data.get('db_input')
+        db_select = cleaned_data.get('db_select')
 
-    # Ensure that the file isn't >10MB. Maybe we'll be able to handle that in the
-    # future, but not now.
+        if not db_input and not db_select:
+            raise forms.ValidationError(
+                'Please select a database or input a new database name.')
+
+    # Ensure that the file isn't >10MB.
     def clean_data_file(self):
         data = self.cleaned_data['data_file']
         if data._size > MAX_UPLOAD_SIZE:
@@ -87,7 +87,7 @@ class DataForm(forms.Form):
             )
         return data
 
-    # Prevent SQL injection by escaping any data that will be passed as 
+    # Prevent SQL injection by escaping any data that will be passed as
     # parameters to the raw SQL query
     def clean_db_input(self):
         data = self._sanitize(self.cleaned_data['db_input'])
@@ -98,12 +98,32 @@ class DataForm(forms.Form):
         return data
 
     data_file = forms.FileField(label='File')
-    table_name = forms.CharField(label='Table name', max_length=100)
-    db_input = forms.CharField(label='Create a new database', max_length=100, required=False)
-    source = forms.CharField(label='Source', max_length=100, required=True)
-    topic = forms.CharField(label='Topic', max_length=100, required=True)
-    next_aquisition = forms.DateField(label='When to update data', widget=forms.SelectDateWidget, required=False)
-    press_contact = forms.CharField(label='Press contact name', max_length=100, required=False)
-    press_contact_number = forms.CharField(label='Press contact number', max_length=100, required=False)
-    press_contact_email = forms.EmailField(label='Press contact email', required=False)
 
+    table_name = forms.CharField(label='Table name',
+                                 max_length=100)
+
+    db_input = forms.CharField(label='Create a new database',
+                               max_length=100,
+                               required=False)
+
+    source = forms.CharField(label='Source',
+                             max_length=100,
+                             required=True)
+
+    topic = forms.CharField(label='Topic',
+                            max_length=100,
+                            required=True)
+
+    next_aquisition = forms.DateField(label='When to update data',
+                                      widget=forms.SelectDateWidget,
+                                      required=False)
+
+    press_contact = forms.CharField(label='Press contact name',
+                                    max_length=100,
+                                    required=False)
+
+    press_contact_number = forms.CharField(label='Press contact number',
+                                           max_length=100, required=False)
+
+    press_contact_email = forms.EmailField(label='Press contact email',
+                                           required=False)
