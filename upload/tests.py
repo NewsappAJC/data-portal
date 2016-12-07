9ab6d9325628cae718fcabcbba7095bfd11036e7
Cox <jonathan.cox.c@gmail.com>
2016-12-07 12:15:22 -0500

    added test for POST to index view

--- upload/tests.py
+++ upload/tests.py
@@ -1,62 +1,51 @@
 # Standard library imports
-import random
-import csv
-from mock import Mock
+import os
 
 # Django imports
-from django.test import TestCase, Client
-from django.core.files import File
+from django.test import TestCase
+# from django.core.files import File
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.conf import settings
+
+# Third-party imports
+# from mock import Mock
 
 # Local module imports
-from .tasks import load_infile
-from .forms import DataForm
+# from .forms import DataForm
 
-#----------------------------------------------------------------
-# Form tests
-#----------------------------------------------------------------
-#class DataFormTestCase(TestCase):
-#    def TestFormValid(self):
-#        test_data = {
-#            table_name: 'voter_dist_data_2016',
-#            db_select: 'user_jcox',
-#            source: 'Secretary of State',
-#            topic: 'Elections',
-#            press_contact: 'Secretary of State Dude',
-#            press_contact_email: 'secretary@secretary.com',
-#            press_contact_number: '123 456 7890',
-#        }
-#
-#        # Generate a mock file for testing purposes.
-#        test_file = Mock(spec=File)
-#
-#        form = DataForm(test_data, test_file)
-#        assert form.is_valid()
 
-#----------------------------------------------------------------
-# View tests
-#----------------------------------------------------------------
 class UploadFileViewTestCase(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='jonathan',
-            email='jonathan.cox.c@gmail.com',
-            password='top_secret')
+        # Create a mock user so that we can access restricted pages
+        # without redirecting to /login/
+        User.objects.create_user(username='jonathan',
+                                 email='jonathan.cox.c@gmail.com',
+                                 password='mock_pw')
+        self.client.login(username='jonathan', password='mock_pw')
 
     def test_index_view_get(self):
         """
-        Test that the index page loads and populates the list of databases in 
+        Test that the index page loads and populates the list of databases in
         the form
         """
-        # Have to set a user or it will redirect to login/
-        self.client.login(username='jonathan', password='top_secret')
-
         response = self.client.get(reverse('upload:index'))
-        print 'Trying to get URL: ', reverse('upload:index')
         self.assertEqual(response.status_code, 200)
 
+    def test_index_view_post(self):
+        test_data = {
+            'table_name': 'voter_dist_data_2016',
+            'db_select': 'user_jcox',
+            'source': 'Secretary of State',
+            'topic': 'Elections',
+            'press_contact': 'Secretary of State Dude',
+            'press_contact_email': 'secretary@secretary.com',
+            'press_contact_number': '123 456 7890',
+        }
 
-#----------------------------------------------------------------
-# S3 tests
-#----------------------------------------------------------------
+        path = os.path.join(settings.BASE_DIR,
+                            'upload', 'test_files', 'vote_data.csv')
 
+        with open(path) as f:
+            test_data['data_file'] = f
+            self.client.post(reverse('upload:index'), test_data)
