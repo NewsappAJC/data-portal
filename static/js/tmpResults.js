ffde5d4b9e6c423ae9177cbf9013a2cc1ac50755
Cox <jonathan.cox.c@gmail.com>
2016-12-08 16:35:46 -0500

    Some explanatory comments

--- static/js/tmpResults.js
+++ static/js/tmpResults.js
@@ -1,15 +1,19 @@
+// Globals
 var csrf,
     $btn;
 
-// Get data from form
+// Event handler for submit button
 $('#file-submit').on('click', function() {
-  // Get the date from the form and send AJAX post request
+  // Get the date from the form and send it as an AJAX post request
   var data = new FormData($('#upload-form')[0]);
   csrf = data.get('csrfmiddlewaretoken');
   postForm(data);
 
+  // Defined as a global so that we can enable/disable from within multiple
+  // functions
   $btn = $(this).button('loading');
 });
+
 
 function postForm(data) {
   $.ajax({
@@ -18,9 +22,12 @@ function postForm(data) {
     contentType: false,
     processData: false,
     cache: false,
+    // We have to add a CSRF token as a header or Django will complain
     headers: {'X-CSRFToken': csrf},
     data: data,
     success: function(res) {
+      // Redirect to the categorize page if form validates and upload to S3
+      // succeeds
       window.location.href = '/categorize/';
     },
     error: function(res) {
@@ -30,23 +37,27 @@ function postForm(data) {
   });
 };
 
+
 function addErrorMessages(errors) {
+  // Clear existing errors
   $('.errors').each(function() {
     $(this).html('');
   });
 
+  // Loop through errors
   $.each(errors, function(err) {
-    // Loop through messages and create a list
     messages = errors[err]
+    // We're only interested in the messages for each error
     fmessages = messages.map(function(m) {
       return m.message;
     });
 
+    // Some errors aren't linked to any particular field
     if  (err === '__all__') {
       $('#non-field-errors').text(fmessages.join(', '));
     }
     else {
-      // Append error messages for each field
+      // Append error messages specified for different field
       var field = $('#id_' + err);
       errorDiv = field.siblings()[1];
       $(errorDiv).text(fmessages.join(', ')); // Use text instead of HTML to prevent XSS
