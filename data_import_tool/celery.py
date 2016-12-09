daf5a307dd1839081cf621d5bfb122925ca3fb7e
Cox <jonathan.cox.c@gmail.com>
2016-12-09 12:53:36 -0500

    better comments

--- data_import_tool/celery.py
+++ data_import_tool/celery.py
@@ -1,10 +1,15 @@
+# Have to use absolute_import so interpreter doesn't get confused by the
+# celery.py file
 from __future__ import absolute_import
 
+# Python standard lib imports
 import os
 
+# Third-party imports
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
+# Django imports
+# set the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_import_tool.settings')
 
 from django.conf import settings
