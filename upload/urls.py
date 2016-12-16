cdf548fb665b1a8c5856b5d2c6ec46059204d4da
Cox <jonathan.cox.c@gmail.com>
2016-12-16 17:00:13 -0500

    added detail view to urlconf

--- upload/urls.py
+++ upload/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     url(r'^categorize/$', views.categorize, name='categorize'),
     url(r'^upload/$', views.write_to_db, name='write_to_db'),
     url(r'^check-task-status/$', views.check_task_status, name='check_status'),
-    url(r'^get-presigned-url/(?P<id>[0-9]+)$', views.get_presigned_url, name='get_presigned_url'),
+    url(r'^detail/(?P<id>[0-9]+)/$', views.get_detail, name='get_detail'),
+    url(r'^get-presigned-url/(?P<id>[0-9]+)/$', views.get_presigned_url, name='get_presigned_url'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', views.logout_user, name='logout')
 ]
