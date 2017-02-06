26c8433101ea0c4620ac0edbbda70aabb0a13537
Cox <jonathan.cox.c@gmail.com>
2017-02-06 11:01:00 -0500

    relative path for presigned-url

--- static/js/search.js
+++ static/js/search.js
@@ -3,7 +3,7 @@
 window.onload = function() {
   function getURL(id) {
     $.ajax({
-      url: '/get-presigned-url/' + id + '/',
+      url: './get-presigned-url/' + id + '/',
       type: 'GET',
       success: function(res) {
         window.location.href = res;
