34455143870c7bb7237fdc50a11487d91442d5f4
Cox <jonathan.cox.c@gmail.com>
2017-02-10 14:56:50 -0500

    fixed issues with highlighting on detail page

--- search/templatetags/highlight.py
+++ search/templatetags/highlight.py
@@ -11,7 +11,7 @@ register = template.Library()
 def highlight(value, arg):
     """Wraps search term in highlight span securely to avoid XSS attack"""
     try:
-        arg = arg.replace(r'&quot;', '')
+        arg = re.sub(r'&quot;|"', '', arg)
         exp = re.compile('(.+)?(' + re.escape(arg) + ')(.+)?', re.IGNORECASE)
         if exp.search(value):
             g = exp.search(value)
